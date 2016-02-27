26341e7922fb8eda0634b00b4403a9df3c29ea8a
George Griffiths <georgegriffiths@live.com>
2016-02-27 20:34:20 +0000

    Switch to Spring Boot

---
+++ src/main/java/com/com3014/group1/projectmatching/ApplicationEntry.java
@@ -0,0 +1,20 @@
+
+package com.com3014.group1.projectmatching;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+
+
+
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
+public class ApplicationEntry {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationEntry.class, args);
+    }
+}
---
+++ src/main/java/com/com3014/group1/projectmatching/services/rest/RestHelloService.java
@@ -0,0 +1,20 @@
+package com.com3014.group1.projectmatching.services.rest;
+
+import com.com3014.group1.projectmatching.core.HelloService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class RestHelloService {
+    
+    @Autowired
+    private HelloService hello;
+    
+     @RequestMapping("/hello/{name}")
+    public String sayHello(@PathVariable String name) {
+        return hello.sayHello(name);
+    }
+}
