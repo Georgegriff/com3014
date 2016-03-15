76c62aaa8b90cb7847acedcf405918df1231728c
Sam Waters <samwaters278@gmail.com>
2016-03-15 16:03:42 +0000

    Added change password to password service

--- src/main/java/com/com3014/group1/projectmatching/core/services/PasswordService.java
+++ src/main/java/com/com3014/group1/projectmatching/core/services/PasswordService.java
@@ -15,6 +15,7 @@ import java.security.NoSuchAlgorithmException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.stereotype.Service;
 
 /**
@@ -23,6 +24,8 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class PasswordService {
+
+    private static final int MIN_PASSWORD_LENGTH = 5;
 
     @Autowired
     private PasswordDAO passwordDAO;
@@ -34,8 +37,9 @@ public class PasswordService {
         if (username == null || submittedPassword == null) {
             return false;
         }
+
         UserEntity userEntity = userDAO.findByUsername(username);
-        
+
         if (userEntity != null) {
             try {
                 Password userPassword = passwordDAO.findByUserId(userEntity.getUserId());
@@ -45,6 +49,37 @@ public class PasswordService {
                 Logger.getLogger(PasswordService.class.getName()).log(Level.SEVERE, null, ex);
                 return false;
             }
+        }
+        return false;
+    }
+
+    public void changePassword(String username, String oldPassword, String newPassword) throws AuthenticationException, IllegalArgumentException {
+        if (!validPassword(newPassword)) {
+            // Maybe change to a different type of exception
+            throw new IllegalArgumentException("New password is invalid") {};
+        }
+        if (checkPassword(username, oldPassword)) {
+            UserEntity userEntity = userDAO.findByUsername(username);
+            if (userEntity != null) {
+                try {
+                    Password userPassword = passwordDAO.findByUserId(userEntity.getUserId());
+                    userPassword.setPassword(hashPassword(newPassword));
+                    passwordDAO.save(userPassword);
+                } catch (NoSuchAlgorithmException | UnsupportedEncodingException ex) {
+                    Logger.getLogger(PasswordService.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        } else {
+            throw new AuthenticationException("Unable to authenticate") {};
+        }
+    }
+
+    private boolean validPassword(String password) {
+        if (password == null || password.length() < MIN_PASSWORD_LENGTH) {
+            return false;
+        }
+        if (password.matches(".*[0-9].*") && (password.matches(".*[A-Z].*") || password.matches(".*[a-z].*"))) {
+            return true;
         }
         return false;
     }
